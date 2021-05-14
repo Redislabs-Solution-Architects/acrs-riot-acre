fc459bd24da3ede2bd0d611f5207f4c36f523561
DaShaun <dashaun@redislabs.com>
2021-05-14 12:15:17 -0500

    fix module

--- terraform/main.tf
+++ terraform/main.tf
@@ -49,9 +49,9 @@ module "redis" {
 }
 
 module "acre" {
-  source          = "github.com/redisgeek/acre-terraform-simple"
-  client_id       = var.client_id
-  client_secret   = var.client_secret
-  subscription_id = var.subscription_id
-  tenant_id       = var.tenant_id
+  source           = "./modules/acre"
+  resource_group   = azurerm_resource_group.main.name
+  application_name = local.application_name
+  environment      = local.environment
+  location         = var.location
 }
