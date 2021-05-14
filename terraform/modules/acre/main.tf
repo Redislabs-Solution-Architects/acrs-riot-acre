9fc19066c0ec0710d4317ec10cb586c7554aa4d1
DaShaun <dashaun@redislabs.com>
2021-05-14 12:09:40 -0500

    client config

--- terraform/modules/acre/main.tf
+++ terraform/modules/acre/main.tf
@@ -16,17 +16,18 @@ resource "azurerm_redis_enterprise_database" "redisgeek" {
   clustering_policy   = var.azure_redis_enterprise_database_clustering_policy
 }
 
+data "azurerm_client_config" "current" {
+}
+
 resource "azurerm_resource_group_template_deployment" "acre" {
   name                = format("redisgeek-%s", random_string.acre_name.result)
   resource_group_name = var.resource_group
   deployment_mode     = var.deployment_mode
   template_content = templatefile("${path.module}/ARM/keyvault.json", {
-    clusterName              = azurerm_redis_enterprise_cluster.redisgeek.name,
-    objectId = local.object_id,
-    servicePrincipalObjectId = var.client_id,
-    keyVaultName             = random_string.key_vault_name.result,
-    secretName               = random_string.secret_name.result
+    clusterName  = azurerm_redis_enterprise_cluster.redisgeek.name,
+    objectId     = data.azurerm_client_config.current.client_id,
+    keyVaultName = random_string.key_vault_name.result,
+    secretName   = random_string.secret_name.result
   })
   depends_on = [azurerm_redis_enterprise_database.redisgeek]
-  tags       = merge(var.tags, { owner = local.sp_display_name })
 }
