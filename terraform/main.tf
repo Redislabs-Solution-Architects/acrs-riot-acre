3f720277565ad17d528f4a19929e9e8eacd97e37
DaShaun <dashaun@redislabs.com>
2021-05-14 12:26:28 -0500

    outputs

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 2.56"
+      version = ">= 2.59.0"
     }
   }
   backend "azurerm" {}
--- terraform/modules/acre/main.tf
+++ terraform/modules/acre/main.tf
@@ -31,3 +31,17 @@ resource "azurerm_resource_group_template_deployment" "acre" {
   })
   depends_on = [azurerm_redis_enterprise_database.redisgeek]
 }
+
+data "azurerm_key_vault" "redisgeek" {
+  name                = random_string.key_vault_name.result
+  resource_group_name = var.resource_group
+  depends_on = [
+    azurerm_resource_group_template_deployment.acre
+  ]
+}
+
+data "azurerm_key_vault_secret" "primaryKey" {
+  name         = random_string.secret_name.result
+  key_vault_id = data.azurerm_key_vault.redisgeek.id
+  depends_on   = [data.azurerm_key_vault.redisgeek]
+}
