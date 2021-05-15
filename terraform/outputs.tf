39b097b30c918ca20766cb35a53519e1eaecca8a
DaShaun <dashaun@dashaun.com>
2021-05-15 03:48:48 -0500

    add creds to output

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,3 +2,19 @@ output "application_hostname" {
   value       = module.application.application_hostname
   description = "The Web application URL."
 }
+
+output "acrs_hostname" {
+  value = module.redis.azure_redis_host
+}
+
+output "acrs_password" {
+  value = module.redis.azure_redis_password
+}
+
+output "acre_hostname" {
+  value = module.acre.acre_host
+}
+
+output "acre_password" {
+  value = module.acre.acre_primaryKey
+}
